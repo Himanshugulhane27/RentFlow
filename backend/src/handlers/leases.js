5ec224ff6a92d424504b93be09bb81b71e6ad122
Himanshu Gulhane <himanshugulhane27@gmail.com>
2026-03-23 12:43:52 +0530

    complete lease CRUD on backend with update and delete endpoints

--- backend/src/handlers/leases.js
+++ backend/src/handlers/leases.js
@@ -3,15 +3,20 @@ const leaseService = require('../services/leaseService');
 
 exports.handler = async (event) => {
   try {
-    const { httpMethod, body } = event;
+    const { httpMethod, pathParameters, body } = event;
+    const leaseId = pathParameters?.id;
 
     switch (httpMethod) {
       case 'GET':
-        const leases = await leaseService.getAllLeases();
-        return success(leases);
+        if (leaseId) return success(await leaseService.getLease(leaseId));
+        return success(await leaseService.getAllLeases());
       case 'POST':
-        const newLease = await leaseService.createLease(JSON.parse(body));
-        return success(newLease);
+        return success(await leaseService.createLease(JSON.parse(body)));
+      case 'PUT':
+        return success(await leaseService.updateLease(leaseId, JSON.parse(body)));
+      case 'DELETE':
+        await leaseService.deleteLease(leaseId);
+        return success({ message: 'Lease deleted' });
       default:
         return error('Method not allowed');
     }
--- backend/src/services/leaseService.js
+++ backend/src/services/leaseService.js
@@ -9,20 +9,40 @@ class LeaseService {
   async createLease(data) {
     const lease = new Lease({ ...data, leaseId: uuidv4() });
     lease.validate();
-
-    await dynamodb.put({
-      TableName: LEASES_TABLE,
-      Item: lease
-    }).promise();
-
+    await dynamodb.put({ TableName: LEASES_TABLE, Item: lease }).promise();
     return lease;
   }
 
+  async getLease(leaseId) {
+    const result = await dynamodb.get({ TableName: LEASES_TABLE, Key: { leaseId } }).promise();
+    if (!result.Item) throw new Error('Lease not found');
+    return result.Item;
+  }
+
   async getAllLeases() {
-    const result = await dynamodb.scan({
-      TableName: LEASES_TABLE
-    }).promise();
+    const result = await dynamodb.scan({ TableName: LEASES_TABLE }).promise();
     return result.Items;
+  }
+
+  async updateLease(leaseId, data) {
+    const params = {
+      TableName: LEASES_TABLE,
+      Key: { leaseId },
+      UpdateExpression: 'SET #s = :status, endDate = :endDate, monthlyRent = :monthlyRent',
+      ExpressionAttributeNames: { '#s': 'status' },
+      ExpressionAttributeValues: {
+        ':status': data.status,
+        ':endDate': data.endDate,
+        ':monthlyRent': data.monthlyRent
+      },
+      ReturnValues: 'ALL_NEW'
+    };
+    const result = await dynamodb.update(params).promise();
+    return result.Attributes;
+  }
+
+  async deleteLease(leaseId) {
+    await dynamodb.delete({ TableName: LEASES_TABLE, Key: { leaseId } }).promise();
   }
 }
 
